ae6228a0dc11fbfc0be473815c6c6aa061adcbca
Curtis Martin <curtis@twotoasters.com>
2014-07-17 17:20:27 -0400

    Initial commit

---
+++ wear/src/main/java/com/twotoasters/messageapidemo/MyActivity.java
@@ -0,0 +1,107 @@
+package com.twotoasters.messageapidemo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MyActivity extends Activity {
+
+    private static final long CONNECTION_TIME_OUT_MS = 100;
+    private static final String MESSAGE = "Hello Wear!";
+
+    private GoogleApiClient client;
+    private String nodeId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        initApi();
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                setupWidgets();
+            }
+        });
+    }
+
+    /**
+     * Initializes the GoogleApiClient and gets the Node ID of the connected device.
+     */
+    private void initApi() {
+        client = getGoogleApiClient(this);
+        retrieveDeviceNode();
+    }
+
+    /**
+     * Sets up the button for handling click events.
+     */
+    private void setupWidgets() {
+        findViewById(R.id.btn_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToast();
+            }
+        });
+    }
+
+    /**
+     * Returns a GoogleApiClient that can access the Wear API.
+     * @param context
+     * @return A GoogleApiClient that can make calls to the Wear API
+     */
+    private GoogleApiClient getGoogleApiClient(Context context) {
+        return new GoogleApiClient.Builder(context)
+                .addApi(Wearable.API)
+                .build();
+    }
+
+    /**
+     * Connects to the GoogleApiClient and retrieves the connected device's Node ID. If there are
+     * multiple connected devices, the first Node ID is returned.
+     */
+    private void retrieveDeviceNode() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+                NodeApi.GetConnectedNodesResult result =
+                        Wearable.NodeApi.getConnectedNodes(client).await();
+                List<Node> nodes = result.getNodes();
+                if (nodes.size() > 0) {
+                    nodeId = nodes.get(0).getId();
+                }
+                client.disconnect();
+            }
+        }).start();
+    }
+
+    /**
+     * Sends a message to the connected mobile device, telling it to show a Toast.
+     */
+    private void sendToast() {
+        if (nodeId != null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+                    Wearable.MessageApi.sendMessage(client, nodeId, MESSAGE, null);
+                    client.disconnect();
+                }
+            }).start();
+        }
+    }
+}
